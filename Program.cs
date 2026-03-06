8e36e74263a384d94234c51826ce8167362bbbcd
emanu <emanu@EMANUEL_RAMREZ>
2026-03-05 22:27:34 -0600

    Logica Aplicada!!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
 ﻿// Escriba un programa que pida una contraseña al usuario y continúe solicitándola hasta que sea correcta.
+
+string contraseña;
+
+
+do
+{
+    Console.WriteLine("Ingrese la contraseña");
+    contraseña = Console.ReadLine();
+
+    if (contraseña == "papitas")
+    {
+        Console.WriteLine("Contraseña correcta!! Iniciando Sesion");
+    }
+
+
+
+} while (contraseña != "papitas");
